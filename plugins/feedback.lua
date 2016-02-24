3afbd5fbd5d444f0cde0090b538231cb4078ed1e
mamadtnt <mamadtnt78@gmail.com>
2016-02-24 21:30:53 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -6,7 +6,7 @@ do
  
  
    local text = matches[1]
-   local chat = "chat#id"..158990680 
+   local chat = "chat#id".. 83829432
    --like : local chat = "chat#id"..12345678
    
   local sends = send_msg(chat, fuse, ok_cb, false)
@@ -25,7 +25,6 @@ do
   },
   run = run
  }
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
+--Copyright; mamadtnt
+--Persian Translate; mamadtnt
 --کپی بدون ذکر منبع حرام است
